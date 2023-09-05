2aec9fe0c6cfe16243b9bfb91051d10d7d6f09d8
gschwei <45996277+gschwei@users.noreply.github.com>
2023-09-05 11:10:40 +0200

    Update changeFormatPhobius.py

--- Analysis/Secretion/changeFormatPhobius.py
+++ Analysis/Secretion/changeFormatPhobius.py
@@ -1,13 +1,13 @@
 # created by Gabriel on 16/12/15
 # last modified by Gabriel on 16/12/15
 
-# I change the phobius output format in such a way that it can be read by R.
+# Here, I change the phobius output format in such a way that R can read it.
 
 species = ("Mepe", "Usbr", "Usho", "Usma", "Spsc", "Sprs", "Sprz")
 
 for s in species:
-    with open("/home/schweizerg/Science-Data/Genomes/FungalGenomes6/Analysis/Secretion/" + s + "/" + s + "_phobius_ChangedFormat.csv", "w") as out_file:
-        p = open("/home/schweizerg/Science-Data/Genomes/FungalGenomes6/Analysis/Secretion/"  + s + "/" + s + "_phobius.txt")
+    with open("/path/to/Analysis/Secretion/" + s + "/" + s + "_phobius_ChangedFormat.csv", "w") as out_file:
+        p = open("/path/to/Analysis/Secretion/"  + s + "/" + s + "_phobius.txt")
         lines = p.readlines()
         for line in lines:
             if not line[0:7] == "SEQENCE": # The first line is the header and therefore ignored.
