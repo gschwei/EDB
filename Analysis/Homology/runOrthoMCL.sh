fcaae0dbf6868282f55014885e107231ce6cc647
gschwei <45996277+gschwei@users.noreply.github.com>
2023-09-05 12:19:30 +0200

    Update runOrthoMCL.sh

--- Analysis/Homology/runOrthoMCL.sh
+++ Analysis/Homology/runOrthoMCL.sh
@@ -7,6 +7,6 @@ step="0.1"
 
 for (( g=11; $g<$(bc<<<"10.1/$step"); g++ )); do
     echo "run OrthoMCL with I =" $g
-    mcl ../../../Blast/Blastp/DbMergedSecretomeQuMergedSecretomeShort.csv --abc -I $g -o "Mcl_I"$g"_out.txt"
+    mcl /path/to/DbMergedSecretomeQuMergedSecretomeShort.csv --abc -I $g -o "Mcl_I"$g"_out.txt"
     echo $g 'is done. Go to next value. Bye.' 
 done
