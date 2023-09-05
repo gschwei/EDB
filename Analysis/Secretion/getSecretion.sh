f61829bd3eff12c3a065ce4051718f3a59862899
gschwei <45996277+gschwei@users.noreply.github.com>
2023-09-05 10:47:24 +0200

    Update getSecretion.sh

--- Analysis/Secretion/getSecretion.sh
+++ Analysis/Secretion/getSecretion.sh
@@ -2,7 +2,7 @@
 # last modified by Gabriel on 10/02/17
 
 # Here, I predict secretion with SignalP 4.1, TMHMM 2.0c and store the results.
-# NB: there is a problem with running phobius; therefore, I use this program in its online version.
+# Note: there is a problem with running phobius; therefore, I use this program in its online version.
 
 for g in "Mepe" "Usho" "Usbr" "Usma" "Spsc" "Sprz" "Sprs"
 do   
@@ -12,6 +12,6 @@ do
     echo $g': run SignalP'
     signalp4.1 -f short ../../Data/$g/$g"_valid_prot.fa" > $g/$g"_signalp.txt" 
     echo $g': run TMHMM'
-    /home/schweizerg/Programs/tmhmm/2.0c/bin/tmhmm -short ../../Data/$g/$g"_valid_prot.fa" > $g/$g"_TMHMM.txt"
+    /path/to/Programs/tmhmm/2.0c/bin/tmhmm -short ../../Data/$g/$g"_valid_prot.fa" > $g/$g"_TMHMM.txt"
     echo $g 'is done. Go to next species. Bye.'
 done
